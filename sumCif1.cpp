4bbc02f67ffa3d36ffa0acddd4c9def18a1b5fe1
Cosmin Chaser <cosmin.chaser@gmail.com>
2017-08-20 19:07:28 +0300

    sumCif1

--- sumCif1.cpp
+++ sumCif1.cpp
@@ -4,7 +4,17 @@
 
 using namespace std;
 
+int sumCif(int a, int r){
+  if(a<10) return r+a%10;
+  return sumCif(a/10, r+(a%10));
+}
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  int intrare;
+  string entry;
+  printf("Intrare: ");
+  getline(cin, entry);
+  intrare = atoi(entry.c_str());
+  printf("Rezultat: %d\n", sumCif(intrare, 0));
   return 0;
 }
